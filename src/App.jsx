c1ab40aeab2947fcd47256d99c081913b1e360de
taahahussainkhan <taaha128@gmail.com>
2024-03-01 22:06:06 +0500

    Navbar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar/Navbar'
 
 export default function App() {
   return (
     <div>
-    <Navbar />  
-    Core Pixel
-    
+    <Navbar /> 
     </div>
   )
 }
